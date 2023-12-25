0f822327f35e785a9960e0fcc763d1b1713077fa
Shane Schroll <schroll24@gmail.com>
2023-12-24 19:58:09 -0600

    added recents news posts + cards and layout / added kash crew section / alt background section created

--- front-page.php
+++ front-page.php
@@ -3,5 +3,13 @@
 $context = Timber::get_context();
 $post = Timber::get_post();
 $context['post'] = $post;
+
+$context['recent_news'] = Timber::get_posts([
+    'post_type' => 'news',
+    'posts_per_page' => 3,
+    'order' => 'DESC',
+    'orderby' => 'date'
+]);
+
 $templates = ['pages/home.twig'];
 Timber::render( $templates, $context );
